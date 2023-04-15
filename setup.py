5f4a118f08ba3c91c3d5041d241599f6583ddf18
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-04-15 11:48:24 +0900

    [mod] ディレクトリ構成変更

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name="ezlite", version="0.2.4", packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quick-viz",
+    version="0.2.6",
+    author="t.ibi",
+    author_email="t.ibi@estyle-inc.jp",
+    description="For the razy who wants to write programs with ease.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Taichi-Ibi/ezlite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
